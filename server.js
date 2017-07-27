e1ffb5ed4ed529347b802b146b2c04172ed483ed
Ygor Duarte Lemos Pereira <vntygpe@venturus.local>
2017-07-27 10:09:38 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date();
+    var log = `${now.toISOString().replace('T', ' ')}: ${req.method} ${req.path}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unsable to log in file.');
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    /*res.send('<h1>Hello World.</h1>');
+    res.send({
+        name: 'Ygor',
+        likes: [
+            'Music',
+            'Games',
+            'Programming'
+        ]
+    });
+    */
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hi there, it works'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error handling request'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send('No routes for ' + req);
+});
+
+app.listen(3000, () => {
+    console.log('Serve is up on port 3000');
+});
